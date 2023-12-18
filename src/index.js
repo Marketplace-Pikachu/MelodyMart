76c6a5d62b010be36b9b26c070b41d31b3f884bd
Your Name <you@example.com>
2023-12-18 18:04:03 -0500

    inital boilerplate setup

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App'
+// import { Provider } from 'react-redux';
+//import store from '../store.jsx'
+
+const root = document.getElementById('root')
+const rootInstance = createRoot(root);
+
+rootInstance.render(
+    // <Provider >
+        <App />
+    // </Provider>
+);
