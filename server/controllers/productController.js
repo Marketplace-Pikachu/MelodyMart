03e64d14180df05fa921b3ee88b0cf31c6b6a239
Adrian Insingo <adrianinsingo@gmail.com>
2023-12-19 10:03:17 -0800

    CRUD functionality for product table

--- server/controllers/productController.js
+++ server/controllers/productController.js
@@ -24,6 +24,80 @@ const productController = {
                 error: error.message,
             });
         }
+    },
+
+    async addProduct(req, res, next) {
+        try {
+            const {
+                description,
+                price,
+                category,
+                sold,
+                seller,
+                product_user_id
+            } = req.body;
+
+            const insertQuery = `INSERT INTO Product (Description, Price, Category, Sold, Seller, product_user_id) VALUES ($1, $2, $3, $4, $5, $6)`;
+            const insertParams = [
+                description, 
+                price, 
+                category, 
+                sold,
+                seller,
+                product_user_id
+            ];
+
+            const result = await client.query(insertQuery, insertParams);
+            res.locals.result = result; 
+            return next();
+        } catch (error) {
+            return next({
+                status: 500,
+                error: error,
+            });
+        }
+    },
+
+    async deleteProduct(req, res, next) {
+        try {
+        } catch (error) {
+            
+        }
+    },
+
+    async deleteProduct(req, res, next) {
+        try {
+            const productId = req.params.product_id; // Assuming the product_id is in the URL parameters
+    
+            // Check if productId is valid (optional step)
+            if (!productId) {
+                return next({
+                    status: 400,
+                    error: "Invalid product_id",
+                });
+            }
+    
+            const deleteQuery = "DELETE FROM Product WHERE product_id = $1";
+            const deleteParams = [productId];
+    
+            const result = await client.query(deleteQuery, deleteParams);
+    
+            if (result.rowCount === 0) {
+                // If no rows were deleted, the product with the specified product_id was not found
+                return next({
+                    status: 404,
+                    error: "Product not found",
+                });
+            }
+    
+            res.locals.result = result;
+            return next();
+        } catch (error) {
+            return next({
+                status: 500,
+                error: error,
+            });
+        }
     }
 }
 
--- server/routes/products.js
+++ server/routes/products.js
@@ -3,10 +3,17 @@ const productController = require('../controllers/productController.js');
 const productRouter = express.Router();
 
 productRouter.get('/', productController.getProduct, (req, res) => {
-    console.log(res.locals.products);
     return res.status(200).json(res.locals.products);
     
 });
+
+productRouter.post('/', productController.addProduct, (req, res) => {
+    return res.status(200).json(res.locals.result);
+});
+
+productRouter.delete('/:product_id', productController.deleteProduct, (req, res) => {
+    return res.status(200).json(res.locals.result);
+})
 
 module.exports = { 
     productRouter: productRouter
