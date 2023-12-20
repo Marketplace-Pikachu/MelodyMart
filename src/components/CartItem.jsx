a3955a2bf3563ca1bfd245030da8df78d7c012d8
Adrian Insingo <adrianinsingo@gmail.com>
2023-12-20 14:52:19 -0800

    fixed up cart and purchased items > Co-authored-by: Jordan <jordansjpalmer@gmail.com> Co-authored-by: Adrian <adrianinsingo@gmail.com> Co-authored-by: Rui <rfan1986@gmail.com> Co-authored-by: Harold <haroldreeves@gmail.com>

--- src/components/CartItem.jsx
+++ src/components/CartItem.jsx
@@ -8,21 +8,33 @@ const CartItem = ({ item }) => {
   const balance = useSelector((state) => state.user.balance);
 
   const handlePerchaseItem = () => {
-    dispatch(removeItem(item.id));
-    dispatch(removeFromCart(item.id));
-    dispatch(purchase(item));
+    const itemPrice = item.price;
+    const remainingBalance = balance - itemPrice;
+
+    if (remainingBalance < 0) {
+      alert("Not enough funds, cannot purchase");
+    } else {
+      dispatch(removeItem(item.product_id));
+      dispatch(removeFromCart(item.product_id));
+      dispatch(purchase(item));
+      alert(`${item.product_title} purchased!`);
+    }
   };
 
   const handleRemoveItem = () => {
-    dispatch(removeFromCart(item.id));
+    dispatch(removeFromCart(item.product_id));
+    alert(`${item.product_title} removed from cart`);
+    
   }
 
   return (
     <div className="item" key={item.id}>
-      <h3>{item.name}</h3>
+      <img src={item.image_link} alt={item.product_title} />
+      <h3>{item.product_title}</h3>
       <h4>{'Seller: ' + item.seller}</h4>
       <h4>{item.description}</h4>
-      <h4>{'Price: '+ item.price}</h4>
+      <h4>{'Category: ' + item.category}</h4>
+      <h4>{`Price: $${item.price}`}</h4>
       <button onClick={handlePerchaseItem}>Purchace</button>
       <button onClick={handleRemoveItem}>Remove</button>
     </div>
